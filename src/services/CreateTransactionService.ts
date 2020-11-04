709ad35c0ff27e2b7974cc9ef73965d288b824c0
Pedro <pedro.lobo.unb@gmail.com>
2020-11-04 14:19:58 -0300

    completando desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    if (
+      type.toUpperCase() !== 'income'.toUpperCase() &&
+      type.toUpperCase() !== 'outcome'.toUpperCase()
+    ) {
+      throw Error('Type not allowed.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      // se nao tem dinheiro suficiente
+      throw new Error('Transaction not allowed.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
